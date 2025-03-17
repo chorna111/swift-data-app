f3e3457c2e48dfe30ebeb46f465e50597162ebd2
Anastasiia Chorna <sirius@macbookpro.home>
2025-03-17 01:12:14 +0100

    First commit, setting up the environment

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+const express=require('express')
+const app=express()
+const mongoose=require('mongoose')
+const port=process.env.PORT||8000
+mongoose.connect('mongodb://localhost/swift-data')
+    .then(()=>console.log('Connected to Mongodb'))
+
+
+
+app.use(express.json())
+app.listen(port,()=>console.log('server started'))
+
+app.use(express.static('build'))
